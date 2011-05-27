df74bc7441af192fe619cfafaba926632612fd58
ardanedh <r.schuerch@gmx.ch>
2011-05-27 15:53:07 +0200

    initial version of scripts

---
+++ dhcp-server/lease_notify.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Mandatory configuration:
+# The Mac address of the server to wake
+HOST_TO_WAKE_MAC="00:33:44:33:44"
+
+# Optional configuration:
+# The network interface from which to send the WOL packet 
+SENDER_NETWORK_IFC="eth0"
+
+# If not using etherwake, set the WOL command to use
+WOL_COMMAND="etherwake -i $SENDER_NETWORK_IFC -b $HOST_TO_WAKE_MAC"
+
+# Load user configuration
+if [ -f /etc/default/was ]
+then
+	. /etc/default/was
+fi
+
+COMMAND="$1"
+shift
+IP="$1"
+shift
+MAC="$1"
+
+case $COMMAND in
+commit)
+	logger -p local5.info "commit for IP address $IP, MAC $MAC"
+	`$WOL_COMMAND`
+	;;
+release)
+	logger -p local5.info "release for IP address $IP, MAC $MAC"
+	;;
+expiry)
+	logger -p local5.info "expiry for IP address $IP"
+	;;
+*)
+	logger -p local5.err "Unknown command for $0!"
+	logger -p local5.err "Valid commands are: commit|release|expiry"
+	exit 1
+esac
+
